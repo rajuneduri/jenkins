0378c9ed14dcbb9b7ffe8f5af8c11133c1dcf3e3
rajuneduri <rajuneduri2255@gmail.com>
2025-08-23 08:42:09 +0530

    testing

---
+++ jenkinstesting/src/main/java/com/example/jenkinstesting/JenkinstestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstestingApplication.class, args);
+	}
+
+}
---
+++ jenkinstesting/src/test/java/com/example/jenkinstesting/JenkinstestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
