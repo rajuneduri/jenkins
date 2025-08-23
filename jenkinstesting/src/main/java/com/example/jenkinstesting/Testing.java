1ccda29d1e79af79e46df29a2affd499ccf35a12
rajuneduri <rajuneduri2255@gmail.com>
2025-08-23 08:50:55 +0530

    testing

---
+++ jenkinstesting/src/main/java/com/example/jenkinstesting/Testing.java
@@ -0,0 +1,13 @@
+package com.example.jenkinstesting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testing {
+
+    @GetMapping("/tet")
+    public String testing(){
+        return "Jenkins";
+    }
+}
